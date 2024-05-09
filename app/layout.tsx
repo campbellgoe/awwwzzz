d9fd7a28ed2d0f7f057e6a7019e1fbe0b86843ee
George Campbell <campbell.goe@gmail.com>
2024-05-09 14:24:13 +0100

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<footer>Made by <a href="mailto:george.campbell@massless.ltd">george.campbell@massless.ltd</a> in 2024</footer></body>
+      
     </html>
   );
 }
